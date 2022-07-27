eecb2ee138e0baa78e17d7e4ab64ab0cdeb6251e
adam richens <adamrichens56@gmail.com>
2022-07-26 19:39:54 -0600

    commiting some JS

---
+++ assets/js/page.js
@@ -0,0 +1,22 @@
+console.log('hellos')
+
+// var getUserRepos = function (user) {
+//     var apiUrl = 'https://api.agify.io?name=' + user;
+  
+//     fetch(apiUrl)
+//       .then(function (response) {
+//         if (response.ok) {
+//           console.log(response);
+//          // response.json().then(function (data) {
+//            // console.log(data);
+//           //}
+//           //);
+//         } else {
+//           alert('Error: ' + response.statusText);
+//         }
+//       })
+//       .catch(function (error) {
+//         alert('Unable to connect to agify.io');
+//       });
+//   };
+//   console.log(getUserRepos)
